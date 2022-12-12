8e6fa771319d39b247f9c58376f6e52c47127d01
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2022-12-12 18:21:00 +1300

    Fixed some things yes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./views/**/*.ejs"],
-    mode: "jit",
-    theme: {
-      extend: {},
+  content: ["./views/**/*.ejs"],
+  mode: "jit",
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: { "50": "#45a64e", "100": "#45a64e", "200": "#45a64e", "300": "#45a64e", "400": "#45a64e", "500": "#45a64e", "600": "#45a64e", "700": "#45a64e", "800": "#45a64e", "900": "#45a64e" }
+      }
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
