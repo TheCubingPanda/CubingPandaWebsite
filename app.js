3cbf4682a4afbc1b7a68fe6651ad81120bf07fb7
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2023-01-03 15:29:26 +1300

    added links to buttons

--- app.js
+++ app.js
@@ -16,6 +16,26 @@ app.get('/discord', function(req, res){
     res.redirect('https://discord.gg/nwH7jcjMaT');
 });
 
+app.get('/tiktok', function(req, res){
+    res.redirect('https://www.tiktok.com/@cubingpandadiscord');
+});
+
+app.get('/medium', function(req, res){
+    res.redirect('https://medium.com/@cubingpandacommunity');
+});
+
+app.get('/youtube', function(req, res){
+    res.redirect('https://www.youtube.com/@cubingpandacommunity');
+});
+
+app.get('/github', function(req, res){
+    res.redirect('https://github.com/TheCubingPanda');
+});
+
+app.get('/partnership', function(req, res){
+    res.redirect('https://docs.google.com/forms/d/e/1FAIpQLSd6HfVEvgFws4wAsP_5iWYH2cab-VFA3CiJ98g9nQv8PvAEYQ/viewform?usp=sf_link');
+});
+
 //app.use(express.static('src/dashboard/views/'));
 
 app.listen(3000);
