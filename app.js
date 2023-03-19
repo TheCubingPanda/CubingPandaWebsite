471210d2ab39e59c2fc4cbce4ad2d879a6dc16f8
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2023-03-20 11:42:37 +1300

    Added topic cmd to bot & bot

--- app.js
+++ app.js
@@ -12,20 +12,20 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function(req, res){
-    res.render('index');
+app.get('/', function (req, res) {
+	res.render('index');
 });
 
-app.get('/partnership', function(req, res){
-    res.redirect('https://docs.google.com/forms/d/e/1FAIpQLSd6HfVEvgFws4wAsP_5iWYH2cab-VFA3CiJ98g9nQv8PvAEYQ/viewform?usp=sf_link');
+app.get('/partnership', function (req, res) {
+	res.redirect('https://docs.google.com/forms/d/e/1FAIpQLSd6HfVEvgFws4wAsP_5iWYH2cab-VFA3CiJ98g9nQv8PvAEYQ/viewform?usp=sf_link');
 });
 
-app.get('/discord', function(req, res){
-    res.redirect('https://discord.gg/nwH7jcjMaT');
+app.get('/discord', function (req, res) {
+	res.redirect('https://discord.gg/nwH7jcjMaT');
 });
 
-app.get('/invite', function(req, res){
-    res.redirect('https://discord.com/api/oauth2/authorize?client_id=1082158692906049546&permissions=274878023680&scope=bot');
+app.get('/invite', function (req, res) {
+	res.redirect('https://discord.com/api/oauth2/authorize?client_id=1082158692906049546&permissions=274878023680&scope=bot');
 });
 
 app.listen(3000);
@@ -47,6 +47,51 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
+
+	// Command registration
+	client.commands = new Collection();
+
+	const commandsPath = path.join(__dirname, 'commands');
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+
+	const commands = [];
+
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const command = require(`./commands/${file}`);
+		commands.push(command.data.toJSON());
+	}
+
+	// Construct and prepare an instance of the REST module
+	const rest = new REST({ version: '10' }).setToken(token);
+	// and deploy your commands!
+	(async () => {
+		try {
+			console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+			// The put method is used to fully refresh all commands in the guild with the current set
+			const data = await rest.put(
+				Routes.applicationCommands(client.user.id),
+				{ body: commands },
+			);
+
+			console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		} catch (error) {
+			// And of course, make sure you catch and log any errors!
+			console.error(error);
+		}
+	})();
 });
 
 // Command event handling
@@ -72,51 +117,6 @@ client.on(Events.InteractionCreate, async interaction => {
 	}
 });
 
-// Command registration
-client.commands = new Collection();
-
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
-}
-
-const commands = [];
-
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
-}
-
-// Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(token);
-
-// and deploy your commands!
-(async () => {
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
-
-		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-			Routes.applicationCommands('1082158692906049546'),
-			{ body: commands },
-		);
-
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		// And of course, make sure you catch and log any errors!
-		console.error(error);
-	}
-})();
 
 // Log in to Discord with your client's token
 client.login(token);
--- commands/topic.js
+++ commands/topic.js
@@ -1,11 +1,11 @@
 const { SlashCommandBuilder } = require('discord.js');
-const topic = require('../topics.json');
+const vars = require('../vars.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('topic')
 		.setDescription('Generates a cubing related topic for conversation!'),
 	async execute(interaction) {
-		await interaction.reply(topic.topics[Math.floor(Math.random() * topic.topics.length)]);
+		await interaction.reply(vars.topics[Math.floor(Math.random() * vars.topics.length)]);
 	},
 };
