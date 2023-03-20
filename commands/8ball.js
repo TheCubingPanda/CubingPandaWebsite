0e951b684a829a7b593a7d5c379d4a610ddca6ce
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2023-03-20 17:57:56 +1300

    Added 8ball.js

---
+++ commands/8ball.js
@@ -0,0 +1,50 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('8ball')
+        .setDescription('Ask the 8ball!')
+        .addStringOption(option =>
+            option.setName('question')
+                .setDescription('Your question for the 8ball')),
+
+    async execute(interaction) {
+        const question = await interaction.options.getString('question');
+        console.log(question);
+        if (!await question) return await interaction.reply("No question asked! Try again");
+
+        await interaction.channel.sendTyping();
+        let eightball = [
+            'It is certain.',
+            'It is decidedly so.',
+            'Without a doubt.',
+            'Yes definitely.',
+            'You may rely on it.',
+            'As I see it, yes.',
+            'Most likely.',
+            'Outlook good.',
+            'Yes.',
+            'Signs point to yes.',
+            'Reply hazy try again.',
+            'Ask again later.',
+            'Better not tell you now.',
+            'Cannot predict now.',
+            'Concentrate and ask again.',
+            'Don\'t count on it.',
+            'My reply is no.',
+            'My sources say no.',
+            'Outlook not so good.',
+            'Very doubtful.',
+            'No way.',
+            'Maybe',
+            'The answer is hiding inside you',
+            'No.',
+            '||No||',
+            '||Yes||'
+        ];
+        let index = (Math.floor(Math.random() * Math.floor(eightball.length)));
+        setTimeout(() => {
+            interaction.reply("*" + question + "*\n🎱 \`" + eightball[index] + "\`");
+        }, 750);
+    },
+};
--- commands/topic.js
+++ commands/topic.js
@@ -5,6 +5,7 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('topic')
 		.setDescription('Generates a cubing related topic for conversation!'),
+		
 	async execute(interaction) {
 		await interaction.reply(vars.topics[Math.floor(Math.random() * vars.topics.length)]);
 	},
