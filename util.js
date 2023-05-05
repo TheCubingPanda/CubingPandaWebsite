275824dcf1e01ec1088e2713abb29c0681c25959
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2023-05-06 11:32:52 +1200

    Create utli.js

---
+++ util.js
@@ -0,0 +1,27 @@
+const { EmbedBuilder } = require('discord.js');
+
+const promotions = [
+    'Promotion 1: ...',
+    'Promotion 2: ...',
+    'Promotion 3: ...',
+    // Add more promotion messages here
+];
+
+function getRandomPromotion() {
+    return promotions[Math.floor(Math.random() * promotions.length)];
+}
+
+async function sendEmbedMessage(interaction, title, description) {
+    const embed = new EmbedBuilder()
+        .setTitle(title)
+        .setDescription(description + '\n\n' + getRandomPromotion())
+        .setColor('#2b2d31');
+
+    try {
+        await interaction.reply({ embeds: [embed] });
+    } catch (error) {
+        console.error('Error sending embed message:', error);
+    }
+}
+
+module.exports = { sendEmbedMessage };
