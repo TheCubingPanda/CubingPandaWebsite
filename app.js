29a7db7c5b35a3ff42755d6c2ed2feddf8507dc2
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2023-05-06 12:30:13 +1200

    make new file for Discor dbot

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
+const { startBot } = require('./discord');
 
 /* Website */
 
@@ -36,95 +37,5 @@ app.listen(3000);
 
 console.log("Website online, listening on http://localhost:3000/");
 
-
-/* Discord Bot, can't be bothered doing anything fancy just yet */
-
-const { Client, Events, GatewayIntentBits, REST, Routes, Collection } = require('discord.js');
-
-require('dotenv').config();
-const token = process.env.TOKEN;
-
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-
-	// Command registration
-	client.commands = new Collection();
-
-	const commandsPath = path.join(__dirname, 'commands');
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
-
-	const commands = [];
-
-	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-	for (const file of commandFiles) {
-		const command = require(`./commands/${file}`);
-		commands.push(command.data.toJSON());
-	}
-
-	// Construct and prepare an instance of the REST module
-	const rest = new REST({ version: '10' }).setToken(token);
-	// and deploy your commands!
-	(async () => {
-		try {
-			console.log(`Started refreshing ${commands.length} application (/) commands.`);
-
-			// The put method is used to fully refresh all commands in the guild with the current set
-			const data = await rest.put(
-				Routes.applicationCommands(client.user.id),
-				{ body: commands },
-			);
-
-			console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-		} catch (error) {
-			// And of course, make sure you catch and log any errors!
-			console.error(error);
-		}
-	})();
-});
-
-// Command event handling
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
-
-// err
-client.on(Events.Error, async err => {
-	console.log('there was err' + err);
-});
-
-// Log in to Discord with your client's token
-client.login(token);
+// Start Discord bot
+startBot();
